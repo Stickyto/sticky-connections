502ed34f2a14d47c3ecf06d4a11e3f201a5c6507
James Garner <j@jada.io>
2022-06-27 11:24:04 +0100

    (Dataverse) improve logging; send events to analytics if a call fails

--- src/CONNECTIONS/CONNECTION_DATAVERSE/makeRequest.js
+++ src/CONNECTIONS/CONNECTION_DATAVERSE/makeRequest.js
@@ -22,12 +22,16 @@ module.exports = async function makeRequest (config, method, url, json) {
       json
     }
   )
-  const toReturn = typeof bodyAsString === 'string' ? JSON.parse(bodyAsString) : undefined
 
   global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] config', config)
   global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] method', method)
   global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] url', url)
   global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] json', json)
+  global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] bodyAsString', bodyAsString)
+  global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] typeof bodyAsString', typeof bodyAsString)
+  global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] bodyAsString.length', bodyAsString.length)
+
+  const toReturn = typeof bodyAsString === 'string' && bodyAsString.length > 0 ? JSON.parse(bodyAsString) : undefined
   global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] toReturn', toReturn)
 
   return toReturn
