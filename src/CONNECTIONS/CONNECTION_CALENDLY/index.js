b9a5b68a94e305ac39d13ead837c913f0caf80a1
James Garner <j@jada.io>
2022-12-12 00:23:15 +0000

    Internal: finalise `userType` at `UT_GENERIC_PAY`, update default Calendly link.

--- src/CONNECTIONS/CONNECTION_CALENDLY/index.js
+++ src/CONNECTIONS/CONNECTION_CALENDLY/index.js
@@ -6,6 +6,6 @@ module.exports = new Connection({
   color: '#006BFF',
   logo: cdn => `${cdn}/connections/CONNECTION_CALENDLY.svg`,
   configNames: ['URL'],
-  configDefaults: ['https://calendly.com/sticky/james-1'],
+  configDefaults: ['https://calendly.com/sticky/lets-talk-am'],
   instructionsDone: 'This link will be used as the default.'
 })
