e1fe38b4ad421ad6faf7bd34a8d71c50c3a3ea02
James Garner <j@jada.io>
2023-04-10 23:54:51 +0100

    Integrate PassKit end to end

---
+++ src/CONNECTIONS/CONNECTION_PASSKIT/index.js
@@ -0,0 +1,49 @@
+/* eslint-disable max-len */
+const Connection = require('../Connection')
+const { assert, isUuid } = require('openbox-node-utils')
+const makeRequest = require('./makeRequest')
+const getSessionUrl = require('../../../../openbox-api/src/getSessionUrl/getSessionUrl')
+
+module.exports = new Connection({
+  id: 'CONNECTION_PASSKIT',
+  name: 'PassKit',
+  color: '#303030',
+  logo: cdn => `${cdn}/connections/CONNECTION_PASSKIT.svg`,
+  configNames: ['API key', 'Tier ID', 'Program ID'],
+  configDefaults: ['', '', ''],
+  userIds: ['d7e01f55-a9a0-4b86-8a36-dd13131ee697'],
+  methods: {
+    go: {
+      name: 'Go',
+      logic: async ({ config, connectionContainer, body }) => {
+        const { user } = connectionContainer
+        const [configApiKey, configTierId, configProgramId] = config
+        assert(configApiKey, 'You have not set a valid API key.')
+        assert(configTierId, 'You have not set a valid tier ID.')
+        assert(configProgramId, 'You have not set a valid program ID.')
+
+        const { toThingId, applicationId, sessionId, sessionName } = body
+        assert(isUuid(sessionId), 'sessionId is not a UUID.')
+
+        const { id: memberId } = await makeRequest(
+          configApiKey,
+          'post',
+          'https://api.pub1.passkit.io/members/member',
+          {
+            'tierId': configTierId,
+            'programId': configProgramId,
+            'externalId': [sessionId, user.id].join('.').substring(0, 100),
+            'metaData': {
+              'url': getSessionUrl(global.rdic, { sessionId, toThingId, applicationId })
+            },
+            'person': {
+              'forename': sessionName || 'Member'
+            }
+          }
+        )
+
+        return memberId
+      }
+    }
+  }
+})
