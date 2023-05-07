b942eff18387fcd583f1ba1e013e4f979c3747ec
Danesh Madarbakus <danesh@sticky.to>
2023-05-07 22:11:17 +0100

    Debug/just eat (#20)
    * updating just eat
    
    * added parameters to .launch
    
    * added logs
    
    * updated import menu and saved products to database
    
    * added rdic logs
    
    * merge time: delete `CONNECTIONS/CONNECTION_JUSTEAT/test.js`
    
    ---------
    
    Co-authored-by: James Garner <j@jada.io>

--- src/CONNECTIONS/CONNECTION_JUSTEAT/importMenu.js
+++ src/CONNECTIONS/CONNECTION_JUSTEAT/importMenu.js
@@ -1,8 +1,19 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const { executablePath } = require('puppeteer')
 
 const importMenu = async (link) => {
-  const browser = await puppeteer.launch({ headless: false })
+  global.rdic.logger.log({}, '[CONNECTION_JUSTEAT] [importMenuScraper] startingScraper')
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: executablePath(),
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
+
   const page = await browser.newPage()
+
   const wait = async (time) => await new Promise(r => setTimeout(r, time))
 
   await page.setViewport({ width: 1080, height: 1024 })
@@ -10,122 +21,143 @@ const importMenu = async (link) => {
   await page.goto(link)
 
   const accept = await page.$('button[data-test-id="accept-all-cookies-button"]')
+
+  await page.$eval('*', (el) => el.innerText)
+
   if (accept) {
     await accept.click()
   }
 
-  const buttons = await page.$$('.c-menuItems-item')
-
+  const sections = await page.$$('.c-menuItems-category')
   const menu = []
 
-  for (let i = 0; i < buttons.length; i++) {
-    const isUnavailable = await buttons[i].evaluate(b => {
-      b.click()
-      return b.querySelector('.c-menuItems-price--offline')
-    })
+  for (let i = 0; i < sections.length; i++) {
+    global.rdic.logger.log({}, `[CONNECTION_JUSTEAT] [importMenuScraper] progress ${i + 1}/${sections.length}`)
 
-    if (isUnavailable) {
-      continue
-    }
-
-    await page.waitForSelector('.c-modal-title')
-
-    await wait(500)
-
-    const preOrderModal = await page.$('[data-test-id=collection-and-delivery-preorder-modal]')
-    const orderForLater = preOrderModal ? await preOrderModal.$('[data-test-id=action-button-component]') : undefined
-
-    if (orderForLater) {
-      await orderForLater.click()
-      await wait(500)
-    }
-
-    const itemElement = await page.$('.c-modal-titleContainer')
-    const item = await itemElement.evaluate(y => {
+    const categoryNameElement = await sections[i].$('h2[data-test-id="menu-category-heading"]')
+    const categoryName = await categoryNameElement.evaluate(y => {
       return y.innerText
     })
 
-    const descriptionElement = await page.$('.c-itemSelector-description')
-    let description
-    if (descriptionElement) {
-      description = await descriptionElement.evaluate(_ => _.innerText)
+    const buttons = await sections[i].$$('.c-menuItems-item')
+
+    const category = {
+      category: categoryName,
+      products: []
     }
 
-    const itemLabels = await page.$('.c-itemSelector-labels')
-
-    const isVegetarian = (await (itemLabels ? itemLabels.$('.c-pieIcon--VegetarianSmall') : false)) ? true : null
-    const isSpicy = (await (itemLabels ? itemLabels.$('.c-pieIcon--SpicySmall') : false)) ? true : null
-
-    const priceElement = await page.$('.c-itemSelector-price')
-
-    const price = priceElement ? await priceElement.evaluate(_ => {
-      return parseInt(Array.from(_.childNodes).filter(child => child.nodeName === '#text')[0].wholeText.replace(/\D+/g, ''), 10)
-    }) : 0
-
-    const imageElement = await page.$('.c-itemSelector-imageContainer img')
-    let imageSrc
-
-    if (imageElement) {
-      imageSrc = await imageElement.evaluate(y => {
-        return y.src
+    for (let i = 0; i < buttons.length; i++) {
+      const isUnavailable = await buttons[i].evaluate(b => {
+        b.click()
+        return b.querySelector('.c-menuItems-price--offline')
       })
-    }
 
-    let itemSelctionElements = await page.$$('.c-itemSelector-section')
-
-    itemSelctionElements.map(async itemSelctionElement => {
-      const options = await itemSelctionElement.$$('.c-itemSelector-section-label')
-      if (options[0]) {
-        await options[0].evaluate(option => { option.click() })
+      if (isUnavailable) {
+        continue
       }
-    })
 
-    await wait(500)
+      await page.waitForSelector('.c-modal-title')
 
-    itemSelctionElements = await page.$$('.c-itemSelector-section')
+      await wait(500)
 
-    const cutomOptions = await Promise.resolve(itemSelctionElements.reduce(async (acc, itemSelctionElement, i) => {
-      let optionsList
-      if (itemSelctionElement) {
-        optionsList = await itemSelctionElement.evaluate(_ => {
-          const options = Array.from(_.querySelectorAll('.c-itemSelector-section-name')).map(singleOptionNode => {
-            const optionText = Array.from(singleOptionNode.querySelectorAll('span'))[0].innerText
+      const preOrderModal = await page.$('[data-test-id=collection-and-delivery-preorder-modal]')
+      const orderForLater = preOrderModal ? await preOrderModal.$('[data-test-id=action-button-component]') : undefined
 
-            const priceNode = singleOptionNode.querySelector('.c-itemSelector-section-cost')
+      if (orderForLater) {
+        await orderForLater.click()
+        await wait(500)
+      }
 
-            const price = priceNode ? parseInt(priceNode.innerText.replace(/\D+/g, ''), 10) : 0
+      const itemElement = await page.$('.c-modal-titleContainer')
+      const item = await itemElement.evaluate(y => {
+        return y.innerText
+      })
 
-            return {
-              option: optionText, additionalPrice: price
-            }
-          })
+      const descriptionElement = await page.$('.c-itemSelector-description')
+      let description
+      if (descriptionElement) {
+        description = await descriptionElement.evaluate(_ => _.innerText)
+      }
 
-          return options
+      const itemLabels = await page.$('.c-itemSelector-labels')
+
+      const isVegetarian = (await (itemLabels ? itemLabels.$('.c-pieIcon--VegetarianSmall') : false)) ? true : null
+      const isSpicy = (await (itemLabels ? itemLabels.$('.c-pieIcon--SpicySmall') : false)) ? true : null
+
+      const priceElement = await page.$('.c-itemSelector-price')
+
+      const price = priceElement ? await priceElement.evaluate(_ => {
+        return parseInt(Array.from(_.childNodes).filter(child => child.nodeName === '#text')[0].wholeText.replace(/\D+/g, ''), 10)
+      }) : 0
+
+      const imageElement = await page.$('.c-itemSelector-imageContainer img')
+      let imageSrc
+
+      if (imageElement) {
+        imageSrc = await imageElement.evaluate(y => {
+          return y.src
         })
       }
 
-      return {
-        [i + 1]: optionsList, ...await Promise.resolve(acc)
-      }
-    }, {}))
+      let itemSelctionElements = await page.$$('.c-itemSelector-section')
 
-    menu.push({
-      item,
-      description,
-      price,
-      cutomOptions,
-      imageSrc,
-      allergies: {
-        isVegetarian,
-        isSpicy,
+      itemSelctionElements.map(async itemSelctionElement => {
+        const options = await itemSelctionElement.$$('.c-itemSelector-section-label')
+        if (options[0]) {
+          await options[0].evaluate(option => { option.click() })
+        }
+      })
 
-      }
-    })
+      await wait(500)
 
-    await page.click('[data-test-id=\'close-modal\']')
+      itemSelctionElements = await page.$$('.c-itemSelector-section')
+
+      const cutomOptions = await Promise.resolve(itemSelctionElements.reduce(async (acc, itemSelctionElement, i) => {
+        let optionsList
+        if (itemSelctionElement) {
+          optionsList = await itemSelctionElement.evaluate(_ => {
+            const options = Array.from(_.querySelectorAll('.c-itemSelector-section-name')).map(singleOptionNode => {
+              const optionText = Array.from(singleOptionNode.querySelectorAll('span'))[0].innerText
+
+              const priceNode = singleOptionNode.querySelector('.c-itemSelector-section-cost')
+
+              const price = priceNode ? parseInt(priceNode.innerText.replace(/\D+/g, ''), 10) : 0
+
+              return {
+                option: optionText, additionalPrice: price
+              }
+            })
+
+            return options
+          })
+        }
+
+        return {
+          [i + 1]: optionsList, ...await Promise.resolve(acc)
+        }
+      }, {}))
+
+      category.products.push({
+        item,
+        description,
+        price,
+        cutomOptions,
+        imageSrc,
+        allergies: {
+          isVegetarian,
+          isSpicy,
+
+        }
+      })
+
+      await page.click('[data-test-id=\'close-modal\']')
+    }
+
+    menu.push(category)
   }
 
   browser.close()
+
   return menu
 }
 
--- src/CONNECTIONS/CONNECTION_JUSTEAT/index.js
+++ src/CONNECTIONS/CONNECTION_JUSTEAT/index.js
@@ -3,6 +3,7 @@
 const Connection = require('../Connection')
 const importMenu = require('./importMenu')
 const { assert, isUrl } = require('openbox-node-utils')
+const { Product, ProductCategory } = require('openbox-entities')
 
 module.exports = new Connection({
   id: 'CONNECTION_JUSTEAT',
@@ -16,18 +17,83 @@ module.exports = new Connection({
   methods: {
     importMenu: {
       name: 'Import Menu',
-      logic: async ({ config }) => {
+      logic: async ({ connectionContainer, config }) => {
+        const {
+          rdic,
+          user,
+          createEvent
+        } = connectionContainer
         const [url] = config
 
         assert(isUrl(url), "Please provide your Just Eat menu URL.")
 
-        const menu = await importMenu(url)
+        const badConnection = (e) => {
+          createEvent({
+            type: 'CONNECTION_BAD',
+            userId: user.id,
+            customData: { id: 'CONNECTION_JUSTEAT', message: e.message }
+          })
+        }
+
+        importMenu(url)
+          .then((res) => {
+            (async () => {
+              global.rdic.logger.log({}, '[CONNECTION_JUSTEAT] [importMenu] scraped menu', res)
+
+              try {
+                for (let i = 0; i < res.length; i++) {
+                  const productsToWrite = res[i].products.map(product => new Product(
+                    {
+                      name: product.item,
+                      description: product.description,
+                      currency: user.currency || 'GBP',
+                      price: product.price,
+                      userId: user.id,
+                    }
+                  ))
+
+                  const promises = productsToWrite.map(productToWrite => {
+                    return rdic.get('datalayerRelational').create('products', productToWrite.toDatalayerRelational())
+                  })
+                  await Promise.all(promises)
+
+                  global.rdic.logger.log({}, '[CONNECTION_JUSTEAT] [importMenu] productsToWrite written', productsToWrite)
+
+                  const entity = new ProductCategory(
+                    {
+                      name: res[i].category,
+                      products: productsToWrite.map(product => product.id),
+                      userId: user.id
+                    },
+                    user
+                  )
+
+                  await rdic.get('datalayerRelational').create('product_categories', entity.toDatalayerRelational())
+                  global.rdic.logger.log({}, '[CONNECTION_JUSTEAT] [importMenu] products written to category', entity)
+                }
+
+                global.rdic.logger.log({}, '[CONNECTION_JUSTEAT] [importMenu] success')
+                createEvent({
+                  type: 'CONNECTION_GOOD',
+                  userId: user.id,
+                  customData: { id: 'CONNECTION_JUSTEAT', message: 'MENU IMPORTED' }
+                })
+              } catch (err) {
+                global.rdic.logger.log({}, '[CONNECTION_JUSTEAT] [importMenu] product creation err', err)
+                badConnection(err)
+              }
+            })()
+          })
+          .catch((err) => {
+            global.rdic.logger.log({}, '[CONNECTION_JUSTEAT] [importMenu] err', err)
+            badConnection(err)
+          })
 
         return {
-          url,
-          menu
+          url
         }
       }
     }
   }
 })
+
