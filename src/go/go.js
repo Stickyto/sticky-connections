d94a7f698ff62b69d6e9fc6e33f95ac7ed0392b1
James Garner <j@jada.io>
2023-07-12 07:17:41 +0100

    Snooze "modifiers" (product question options) (#26)
    * Snooze "modifiers" (product question options)
    
    * Injected `getProductCategories` (`connectionContainer`): sort by `created_at` (consistent with products)
    
    * Bump to `7.4.1`

--- src/go/go.js
+++ src/go/go.js
@@ -31,7 +31,7 @@ module.exports = async function go (connection, method, { rdic, user, partner, b
       await rdic.get('datalayerRelational').create('events', event.toDatalayerRelational())
     },
     getProducts: async (rdic, user, query = {}) => {
-      const rawEntities = await rdic.get('datalayerRelational').read('products', { user_id: user.id, ...query })
+      const rawEntities = await rdic.get('datalayerRelational').read('products', { user_id: user.id, ...query }, 'created_at ASC')
       return rawEntities.map(re => new Product().fromDatalayerRelational(re))
     },
     createProduct: async (...args) => {
@@ -44,7 +44,7 @@ module.exports = async function go (connection, method, { rdic, user, partner, b
     },
 
     getProductCategories: async (rdic, user, query = {}) => {
-      const rawEntities = await rdic.get('datalayerRelational').read('product_categories', { user_id: user.id, ...query })
+      const rawEntities = await rdic.get('datalayerRelational').read('product_categories', { user_id: user.id, ...query }, 'created_at ASC')
       return rawEntities.map(re => new ProductCategory(undefined, user).fromDatalayerRelational(re))
     },
     createProductCategory: async (...args) => {
