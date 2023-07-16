aca892c7675070b0640c4183b32d75b5627b5760
James Garner <j@jada.io>
2023-07-16 01:40:50 +0100

    Comment out any EP methods that aren't fully tested yet

--- src/CONNECTIONS/CONNECTION_ELITE_DYNAMICS/index.js
+++ src/CONNECTIONS/CONNECTION_ELITE_DYNAMICS/index.js
@@ -32,33 +32,6 @@ module.exports = new Connection({
   configNames: ['Client ID', 'Client Secret', 'Scope', 'OAuth URL', 'Code unit URL'],
   configDefaults: ['', '', 'https://api.businesscentral.dynamics.com/.default', 'https://login.microsoftonline.com/---/oauth2/v2.0/token', 'https://api.businesscentral.dynamics.com/v2.0/---/Sandbox/WS/Customer-Name/Codeunit'],
   methods: {
-    bookingAuthenticate: {
-      name: 'Booking > Authenticate',
-      logic: async ({ config }) => {
-        const body = getBody('BookingAPI', 'GetSetup', {})
-        const xmlResponse = await makeRequest(body, config, 'BookingAPI')
-        return xmlResponse.GetSetup
-      }
-    },
-    bookingCreate: {
-      name: 'Booking > Create',
-      logic: async ({ config }) => {
-        const body = getBody('BookingAPI', 'CreateBooking', {})
-        const xmlResponse = await makeRequest(body, config, 'BookingAPI')
-        const { booking_no: id } = xmlResponse.CreateBooking
-        return {
-          id
-        }
-      }
-    },
-    bookingUpdate: {
-      name: 'Booking > Update',
-      logic: async ({ config, body }) => {
-        const reqBody = getBody('BookingAPI', 'UpdateNotes', { 'booking_no': body.bookingId, notes: body.text })
-        await makeRequest(reqBody, config, 'BookingAPI')
-        return {}
-      }
-    },
     bookingGet: {
       name: 'Booking > Get',
       logic: async ({ connectionContainer, config, body }) => {
@@ -71,12 +44,10 @@ module.exports = new Connection({
           no_of_adults: countAdults,
           no_of_children: countChildren,
           notes,
-          // total_price,
-          // amount_paid,
-          outstanding_amount: total
+          total_price: total,
+          // amount_paid: total,
+          // outstanding_amount: total
         } = asJsonObject
-        // total_price = Math.trunc(parseFloat(total_price) * 100)
-        // amount_paid = Math.trunc(parseFloat(amount_paid) * 100)
         total = Math.trunc(parseFloat(total) * 100)
         countAdults = parseInt(countAdults, 10)
         countChildren = parseInt(countChildren, 10)
@@ -84,10 +55,10 @@ module.exports = new Connection({
           id,
           dates: {
             create: dateStringToUtc(asJsonObject.added_date),
-            // start: dateStringToUtc(asJsonObject.arrival_date),
-            // end: dateStringToUtc(asJsonObject.departure_date),
-            start: 1644192000 || dateStringToUtc(asJsonObject.arrival_date),
-            end: 1644537600 || dateStringToUtc(asJsonObject.departure_date)
+            start: dateStringToUtc(asJsonObject.arrival_date),
+            end: dateStringToUtc(asJsonObject.departure_date),
+            // start: 1644192000 || dateStringToUtc(asJsonObject.arrival_date),
+            // end: 1644537600 || dateStringToUtc(asJsonObject.departure_date)
           },
           times: {
             checkIn: dateStringToUtc(asJsonObject.arrival_date) + timeStringToSeconds(asJsonObject.check_in_time) - user.timezone,
@@ -95,7 +66,7 @@ module.exports = new Connection({
           },
           properties: {
             checkedIn: asJsonObject.arrived !== 'false',
-            checkedOut: true || asJsonObject.departed !== 'false',
+            checkedOut: asJsonObject.departed !== 'false',
             cancelled: asJsonObject.cancelled !== 'false'
           },
           permissions: {
@@ -123,52 +94,79 @@ module.exports = new Connection({
         }
       }
     },
-    bookingCheckIn: {
-      name: 'Booking > Check in',
-      logic: async ({ config, body }) => {
-        const reqBody = getBody('BookingAPI', 'CheckInBooking', { 'booking_no': body.bookingId })
-        await makeRequest(reqBody, config, 'BookingAPI')
-        return {}
-      }
-    },
-    bookingCheckOut: {
-      name: 'Booking > Check out',
-      logic: async ({ config, body }) => {
-        const reqBody = getBody('BookingAPI', 'CheckOutBooking', { 'booking_no': body.bookingId })
-        await makeRequest(reqBody, config, 'BookingAPI')
-        return {}
-      }
-    },
-    bookingPayStart: {
-      name: 'Booking > Pay start',
-      logic: async ({ config, body }) => {
-        const reqBody = getBody('BookingAPI', 'TagBooking', { 'booking_no': body.bookingId })
-        await makeRequest(reqBody, config, 'BookingAPI')
-        return {}
-      }
-    },
-    bookingPayGood: {
-      name: 'Booking > Pay good',
-      logic: async ({ config, body }) => {
-        const reqBody1 = getBody('BookingAPI', 'MakePayment', { 'booking_no': body.bookingId, reference: body.paymentId.substring(0, 30), amount: (body.total / 100).toFixed(2) })
-        const xmlResponse = await makeRequest(reqBody1, config, 'BookingAPI')
-        let { receipt_no: paymentGatewayId } = xmlResponse.MakePayment
-        if (body.doConfirm) {
-          const reqBody2 = getBody('BookingAPI', 'ConfirmBooking', { 'booking_no': body.bookingId })
-          await makeRequest(reqBody2, config)
-        }
-        return {
-          paymentGatewayId
-        }
-      }
-    },
-    bookingPayBad: {
-      name: 'Booking > Pay bad',
-      logic: async ({ config, body }) => {
-        const reqBody = getBody('BookingAPI', 'UntagBooking', { 'booking_no': body.bookingId })
-        await makeRequest(reqBody, config, 'BookingAPI')
-        return {}
-      }
-    }
+    // bookingAuthenticate: {
+    //   name: 'Booking > Authenticate',
+    //   logic: async ({ config }) => {
+    //     const body = getBody('BookingAPI', 'GetSetup', {})
+    //     const xmlResponse = await makeRequest(body, config, 'BookingAPI')
+    //     return xmlResponse.GetSetup
+    //   }
+    // },
+    // bookingCreate: {
+    //   name: 'Booking > Create',
+    //   logic: async ({ config }) => {
+    //     const body = getBody('BookingAPI', 'CreateBooking', {})
+    //     const xmlResponse = await makeRequest(body, config, 'BookingAPI')
+    //     const { booking_no: id } = xmlResponse.CreateBooking
+    //     return {
+    //       id
+    //     }
+    //   }
+    // },
+    // bookingUpdate: {
+    //   name: 'Booking > Update',
+    //   logic: async ({ config, body }) => {
+    //     const reqBody = getBody('BookingAPI', 'UpdateNotes', { 'booking_no': body.bookingId, notes: body.text })
+    //     await makeRequest(reqBody, config, 'BookingAPI')
+    //     return {}
+    //   }
+    // },
+    // bookingCheckIn: {
+    //   name: 'Booking > Check in',
+    //   logic: async ({ config, body }) => {
+    //     const reqBody = getBody('BookingAPI', 'CheckInBooking', { 'booking_no': body.bookingId })
+    //     await makeRequest(reqBody, config, 'BookingAPI')
+    //     return {}
+    //   }
+    // },
+    // bookingCheckOut: {
+    //   name: 'Booking > Check out',
+    //   logic: async ({ config, body }) => {
+    //     const reqBody = getBody('BookingAPI', 'CheckOutBooking', { 'booking_no': body.bookingId })
+    //     await makeRequest(reqBody, config, 'BookingAPI')
+    //     return {}
+    //   }
+    // },
+    // bookingPayStart: {
+    //   name: 'Booking > Pay start',
+    //   logic: async ({ config, body }) => {
+    //     const reqBody = getBody('BookingAPI', 'TagBooking', { 'booking_no': body.bookingId })
+    //     await makeRequest(reqBody, config, 'BookingAPI')
+    //     return {}
+    //   }
+    // },
+    // bookingPayGood: {
+    //   name: 'Booking > Pay good',
+    //   logic: async ({ config, body }) => {
+    //     const reqBody1 = getBody('BookingAPI', 'MakePayment', { 'booking_no': body.bookingId, reference: body.paymentId.substring(0, 30), amount: (body.total / 100).toFixed(2) })
+    //     const xmlResponse = await makeRequest(reqBody1, config, 'BookingAPI')
+    //     let { receipt_no: paymentGatewayId } = xmlResponse.MakePayment
+    //     if (body.doConfirm) {
+    //       const reqBody2 = getBody('BookingAPI', 'ConfirmBooking', { 'booking_no': body.bookingId })
+    //       await makeRequest(reqBody2, config)
+    //     }
+    //     return {
+    //       paymentGatewayId
+    //     }
+    //   }
+    // },
+    // bookingPayBad: {
+    //   name: 'Booking > Pay bad',
+    //   logic: async ({ config, body }) => {
+    //     const reqBody = getBody('BookingAPI', 'UntagBooking', { 'booking_no': body.bookingId })
+    //     await makeRequest(reqBody, config, 'BookingAPI')
+    //     return {}
+    //   }
+    // }
   }
 })
