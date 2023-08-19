88a7ef8c15afbf8159fb52aacbc5880150eaa0e6
James Garner <j@jada.io>
2023-08-19 09:39:04 +0100

    `CONNECTION_CYCLR` is ready for prime time!

--- src/CONNECTIONS/CONNECTION_DATAVERSE/makeRequest.js
+++ src/CONNECTIONS/CONNECTION_DATAVERSE/makeRequest.js
@@ -22,10 +22,7 @@ module.exports = async function makeRequest(config, method, url, json) {
     }
   )
 
-  global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] config', config)
-  global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] method', method)
-  global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] url', url)
-  global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest] json', json)
+  global.rdic.logger.log({}, '[CONNECTION_DATAVERSE] [makeRequest]', { method, url, config, json })
 
   try {
     const body = await response.json()
