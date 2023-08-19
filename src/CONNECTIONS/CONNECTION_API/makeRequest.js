b1556c46a66cbc6e4e62a6f5ac1f33deee4dd949
James Garner <j@jada.io>
2023-08-19 08:52:19 +0100

    refactor rubbish `CONNECTION_API`; this was rubbish architecture from me

--- src/CONNECTIONS/CONNECTION_API/makeRequest.js
+++ src/CONNECTIONS/CONNECTION_API/makeRequest.js
@@ -1,13 +1,12 @@
-/* eslint-disable max-len */
 module.exports = async function makeRequest(privateKey, method, url, json) {
-  // global.rdic.logger.log({}, '[CONNECTION_API] [makeRequest] privateKey', privateKey)
-  // global.rdic.logger.log({}, '[CONNECTION_API] [makeRequest] method', method)
-  // global.rdic.logger.log({}, '[CONNECTION_API] [makeRequest] url', url)
-  // global.rdic.logger.log({}, '[CONNECTION_API] [makeRequest] json', json)
-
-  const headers = privateKey ? {
-    'Authorization': `Bearer ${privateKey}`
-  } : {}
+  const headers = privateKey ?
+    {
+      'content-type': 'application/json',
+      'authorization': `Bearer ${privateKey}`
+    } :
+    {
+      'content-Type': 'application/json'
+    }
 
   const response = await fetch(url,
     {
@@ -16,15 +15,11 @@ module.exports = async function makeRequest(privateKey, method, url, json) {
       body: JSON.stringify(json)
     }
   )
-
-  try {
-    const body = await response.json()
-    global.rdic.logger.log({}, '[CONNECTION_API] [makeRequest] body', body)
-
-    return body
-  } catch (e) {
-    global.rdic.logger.log({}, '[CONNECTION_API] [makeRequest] error', e.message)
-
-    return undefined
+  if (!response.ok) {
+    throw new Error(`!response.ok: [${url}]: ${await response.text()}`)
   }
+
+  const asJson = await response.json()
+  global.rdic.logger.log({}, '[CONNECTION_API] [makeRequest]', { asJson })
+  return asJson
 }
