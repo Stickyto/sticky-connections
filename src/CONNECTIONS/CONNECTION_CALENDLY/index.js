bdd20bcf506b00cd5fb0214748d8ac92ec32886b
James Garner <j@jada.io>
2023-12-05 08:10:55 +0000

    Hotfix: Default Calendly link

--- src/CONNECTIONS/CONNECTION_CALENDLY/index.js
+++ src/CONNECTIONS/CONNECTION_CALENDLY/index.js
@@ -6,5 +6,5 @@ module.exports = new Connection({
   color: '#006BFF',
   logo: cdn => `${cdn}/connections/CONNECTION_CALENDLY.svg`,
   configNames: ['Default URL for flow steps'],
-  configDefaults: ['https://calendly.com/sticky/lets-talk-am']
+  configDefaults: ['https://calendly.com/sticky/talk-to-us']
 })
