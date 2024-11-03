296f6851a1bcec747ec07e992cefa7cbfb7d97c9
James Garner <j@jada.io>
2024-11-03 17:25:04 +0000

    Support Trustist 2fa URLs

--- src/CONNECTIONS/CONNECTION_SENDGRID_INBOUND/index.js
+++ src/CONNECTIONS/CONNECTION_SENDGRID_INBOUND/index.js
@@ -13,21 +13,29 @@ module.exports = new Connection({
     'inbound-ffe73115-22a5': {
       name: 'inbound-ffe73115-22a5',
       logic: async calledWith => {
-        const [leftHandSide] = calledWith.body.to.split('@')
-        const [userId, userIdRest] = leftHandSide.split('---')
-        assert(isUuid(userId), `Please send emails to [user-id]---[...]@[...] (userId=${userId})`)
-
-        const regex = /https?:\/\/[^\s"'>]+/g
-        const urls = calledWith.body.html.match(regex) || []
-        const url = urls.find(u => u.startsWith('https://partners.trustist.com'))
-
-        assert(url, `Could not find a Trustist URL! (urls.length=${urls.length})`)
-
-        await calledWith.connectionContainer.createEvent({
-          type: 'CONNECTION_GOOD',
-          userId,
-          customData: { id: 'CONNECTION_SENDGRID_INBOUND', method: 'inbound-ffe73115-22a5', theirId: url }
-        })
+        try {
+          const [leftHandSide] = calledWith.body.to.split('@')
+          const [userId, userIdRest] = leftHandSide.split('---')
+          assert(isUuid(userId), `Please send emails to [user-id]---[...]@[...] (userId=${userId})`)
+  
+          const regex = /https?:\/\/[^\s"'>]+/g
+          const urls = calledWith.body.html.match(regex) || []
+          const url = urls.find(u => u.startsWith('https://partners.trustist.com'))
+  
+          assert(url, `Could not find a Trustist URL! (urls.length=${urls.length})`)
+  
+          await calledWith.connectionContainer.createEvent({
+            type: 'CONNECTION_GOOD',
+            userId,
+            customData: { id: 'CONNECTION_SENDGRID_INBOUND', method: 'inbound-ffe73115-22a5', theirId: url }
+          })
+        } catch ({ message }) {
+          await calledWith.connectionContainer.createEvent({
+            type: 'CONNECTION_BAD',
+            userId: calledWith.connectionContainer.user.id,
+            customData: { id: 'CONNECTION_SENDGRID_INBOUND', message: `inbound-ffe73115-22a5: ${message}` }
+          })
+        }
       }
     }
   }
