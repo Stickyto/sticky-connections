bbec9d28d03143c25e4064d2507ec57c5c5efcec
James <j@jada.io>
2025-12-22 23:11:27 +0000

    Guesty: Be ready to log webhook data

--- src/CONNECTIONS/CONNECTION_GUESTY/index.js
+++ src/CONNECTIONS/CONNECTION_GUESTY/index.js
@@ -1,19 +1,5 @@
 const Connection = require('../Connection')
 
-async function eventHookLogic (config, connectionContainer) {
-  const { user, application, payment, createEvent } = connectionContainer
-
-  try {
-  } catch (e) {
-    createEvent({
-      type: 'CONNECTION_BAD',
-      userId: user.id,
-      applicationId: application ? application.id : undefined,
-      customData: { id: 'CONNECTION_GUESTY', message: e.message }
-    })
-  }
-}
-
 module.exports = new Connection({
   id: 'CONNECTION_GUESTY',
   name: 'Guesty',
@@ -22,7 +8,14 @@ module.exports = new Connection({
   logo: cdn => `${cdn}/connections/CONNECTION_GUESTY.svg`,
   configNames: [],
   configDefaults: [],
-  eventHooks: {
-    'SESSION_CART_PAY': eventHookLogic
+  methods: {
+    'reservation': {
+      name: 'Reservation',
+      logic: async ({ connectionContainer, config, body }) => {
+        const { user } = connectionContainer
+        global.rdic.logger.log({ user }, '[CONNECTION_GUESTY] [methods->reservation]', body)
+        return 'methods->reservation is working!'
+      }
+    }
   }
 })
