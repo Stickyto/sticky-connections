b63ece5b0e9b234ee4bb08ac8b6cb434ed40829d
James <j@jada.io>
2026-02-22 01:17:44 +0000

    `CONNECTION_ECOMMPAY`: disable `CONNECTION_TYPE_OCT`

--- src/CONNECTIONS/CONNECTION_ECOMMPAY/index.js
+++ src/CONNECTIONS/CONNECTION_ECOMMPAY/index.js
@@ -50,7 +50,6 @@ async function makeRequest (url, payload, secretKey) {
 
 module.exports = new Connection({
   id: 'CONNECTION_ECOMMPAY',
-  type: 'CONNECTION_TYPE_OCT',
   name: 'Ecommpay',
   color: '#000000',
   logo: cdn => `${cdn}/connections/CONNECTION_ECOMMPAY.svg`,
